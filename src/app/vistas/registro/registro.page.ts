feca9719f7830eabc2a63d64a159d2668000fc85
DarvX01 <dreycoarak00@gmail.com>
2024-08-27 12:49:18 -0400

    algunos arreglos para registro moverte y validacion

--- src/app/vistas/registro/registro.page.ts
+++ src/app/vistas/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+/* aqui podemos crear una variable 
+
+  NOMBRE: TIPO = VALOR;  
+  NOMBRE = CLASE(VALOR);   */
+
+  persona = new FormGroup({
+    rut: new FormControl('', [Validators.required, Validators.pattern("[0-9]{7,8}-[0-9kK]{1}")]),
+    nombre: new FormControl('', [Validators.minLength(3), Validators.required, Validators.pattern("[a-z]{3,10}")]),
+    fecha_nacimiento: new FormControl(),
+    genero: new FormControl()
+  });
+
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+  registrar():void{
+    /* validaciones o guardar datos llamar a un DAO? conversión */
+    console.log(this.persona.value);
+    alert("registrado");
+    this.router.navigate(['/home']);
+    
+  }
+
 
 }
